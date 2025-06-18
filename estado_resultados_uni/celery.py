2499dd44b9027d8d02050a63597df22613978e63
Vale2704 <valenietzsche@gmail.com>
2025-06-17 18:44:55 -0600

    Actualizar configuración de Celery

--- estado_resultados_uni/celery.py
+++ estado_resultados_uni/celery.py
@@ -2,7 +2,7 @@ import os
 from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'estado_resultados_uni.settings')
-
+ 
 app = Celery('estado_resultados_uni')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks() 
